d952cb0c7fa39ae7986b8a5e8be7413703765e70
hamasasi12 <hamasakifsanie@gmail.com>
2025-11-04 09:16:08 +0800

    adding API Route

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/asesi/payments/notification', [PaymentController::class, 'notification'])->name('payments.notification');
+Route::post('/payments/notification', [PaymentController::class, 'notification'])->name('payments.notification');
+
+
